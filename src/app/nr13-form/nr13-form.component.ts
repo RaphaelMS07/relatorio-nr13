a8c6d71371e7b93166527f8476a26bc3ebdf9f34
Dazzlelator <raphaelms07@gmail>
2023-08-29 22:37:58 -0300

    Criando formulario nr-13

--- src/app/nr13-form/nr13-form.component.ts
+++ src/app/nr13-form/nr13-form.component.ts
@@ -11,9 +11,38 @@ export class Nr13FormComponent implements OnInit {
 
   constructor() { }
 
-  nr13 = new FormGroup({})
+  nr13 = new FormGroup({
+    nome: new FormControl(""),
+    cnpj: new FormControl(""),
+    endereco: new FormControl(""),
+    reservatorioCheck: new FormControl(false),
+    redeCheck: new FormControl(false),
+    caldeiraCheck: new FormControl(false),
+    modelo: new FormControl(""),
+    AnoFabricacao: new FormControl(""),
+    numSerie: new FormControl(""),
+    codProjeto: new FormControl(""),
+    anoEdicao: new FormControl(""),
+    pressTeste: new FormControl(""),
+    pressTrab: new FormControl(""),
+    exameVisual: new FormControl(""),
+    ultrassomCheck: new FormControl(false),
+    espessuraChapa: new FormControl(),
+    hidrostatico: new FormControl(),
+    imgFileEquipamento: new FormControl(),
+    imgFileBombaTeste: new FormControl(),
+    resultadoTeste: new FormControl("") //a continuar
+
+
+  })
+
+
 
   ngOnInit(): void {
+  }
+
+  submitForm(){
+    window.alert("form enviado")
   }
 
 }
